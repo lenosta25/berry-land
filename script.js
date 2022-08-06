4e03e1a28bfd8367dc09eae36dd01d06e35d8e3e
Lena <93406015+lenosta25@users.noreply.github.com>
2022-08-06 16:53:07 +0300

    added a section with products

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Burger
 const burger = document.querySelector('.menu__burger');
 const menu = document.querySelector('.menu__body');
 const body = document.body
@@ -7,5 +8,15 @@ if (burger && menu) {
 		burger.classList.toggle('_active');
 		menu.classList.toggle('_active');
 		body.classList.toggle('_lock');
+	})
+}
+// Tabs
+let tab = document.querySelectorAll('.shop-tabs__item');
+for (let i = 0; i < tab.length; i++) {
+	tab[i].addEventListener('click', function () {
+		for (let j = 0; j < tab.length; j++) {
+			tab[j].classList.remove('_active');
+		}
+		this.classList.add('_active');
 	})
 }
